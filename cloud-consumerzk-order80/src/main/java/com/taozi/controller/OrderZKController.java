4bebe7b72e5d29f8600edc0e98f5b3050fa444a2
S9037218 <Orangejuice135!>
2021-04-20 17:59:33 +0800

    ""

---
+++ cloud-consumerzk-order80/src/main/java/com/taozi/controller/OrderZKController.java
@@ -0,0 +1,25 @@
+package com.taozi.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderZKController {
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymentInfo() {
+        String res = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        return res;
+    }
+}
