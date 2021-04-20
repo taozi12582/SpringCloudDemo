99665f5e7aec13ce0fb5b96eecf185dad2f6a457
S9037218 <Orangejuice135!>
2021-04-20 17:18:16 +0800

    a

---
+++ cloud-consumer-order80/src/main/java/com/taozi/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.taozi.controller;
+
+import com.taozi.entities.CommonResult;
+import com.taozi.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+    public static final String PAYMENT_RUL = "http://CLOUD-PROVIDER-SERVICE";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_RUL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_RUL + "/payment/get/" + id, CommonResult.class);
+    }
+}
