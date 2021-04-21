af9c73be21e2fa6db4ab8af9fcc17796afff66f8
S9037218 <Orangejuice135!>
2021-04-21 14:02:34 +0800

    ""

---
+++ cloud-providerconsul-payment8006/src/main/java/com/taozi/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.taozi.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentZK() {
+        return "springCloud with consul: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
