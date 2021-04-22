d84a762ea85f1de883579907ba1978f05394bace
S9037218 <Orangejuice135!>
2021-04-22 14:35:40 +0800

    gateway断言

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zbj = ZonedDateTime.now();
+        System.out.println(zbj);
+//        2021-04-22T14:19:21.860+08:00[Asia/Shanghai]
+    }
+}
