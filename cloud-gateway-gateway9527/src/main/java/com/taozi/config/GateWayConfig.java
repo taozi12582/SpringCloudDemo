48651c792913b090754c3e905fbafbdfeefa3de7
S9037218 <Orangejuice135!>
2021-04-22 14:13:13 +0800

    gateway动态路由平衡功能

---
+++ cloud-gateway-gateway9527/src/main/java/com/taozi/config/GateWayConfig.java
@@ -0,0 +1,21 @@
+package com.taozi.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_taozi_1",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+        routes.route("path_route_taozi_2",
+                r -> r.path("/guoji")
+                        .uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
